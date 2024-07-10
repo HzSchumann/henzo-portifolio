a6ce79df7697a7f72288512da944b9ea7e12e101
Henzo Schumann <henzoschumann@hotmail.com>
2024-07-09 21:59:01 -0300

    home page creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    CertificationsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
